764fde0b1649301fab7ad83de5d929f2c6e752a1
mattc2307 <118995267+mattc2307@users.noreply.github.com>
2023-05-07 21:44:37 +0100

    46%

--- image-classification.py
+++ image-classification.py
@@ -1,93 +1,108 @@
-import matplotlib.pyplot as plt
-import numpy as np
-import PIL
 import tensorflow as tf
-
-
-from tensorflow import keras
-from tensorflow.keras import layers
-from tensorflow.keras.models import Sequential
-
-import tensorflow_hub as hub
 import tensorflow_datasets as tfds
+from tensorflow import keras 
+from tensorflow.keras.models import Model, Sequential
+from tensorflow.keras.layers import *
+from tensorflow.keras.callbacks import ReduceLROnPlateau
+# Load the dataset
+train_data, test_data ,valid_data= tfds.load(name='oxford_flowers102', split=['train', 'test','validation'], as_supervised=True)
 
-import pathlib 
+# Define the image size and batch size
+img_size = (224, 224)
+batch_size = 32
 
-dataset, info = tfds.load('oxford_flowers102', with_info=True, as_supervised=True)
+# Define the data augmentation pipeline
+train_data = train_data.map(lambda x, y: (tf.image.central_crop(x, central_fraction=0.8), y))
 
-# Create a training set, a validation set and a test set.
-test_ds, train_ds, validate_ds = dataset['test'], dataset['train'], dataset['validation']
+train_data = train_data.map(lambda x, y: (tf.image.resize(x, img_size), y))
+train_data = train_data.map(lambda x, y: (tf.image.random_flip_left_right(x), y))
 
-num_train = len(train_ds)
-num_val = len(validate_ds)
-num_test =len(test_ds)
-num_classes = info.features['label'].num_classes
+train_data = train_data.shuffle(buffer_size=1024)
+train_data = train_data.batch(batch_size)
+train_data = train_data.prefetch(tf.data.AUTOTUNE)
 
+test_data = test_data.map(lambda x, y: (tf.image.central_crop(x, central_fraction=0.8), y))
 
+test_data = test_data.map(lambda x, y: (tf.image.resize(x, img_size), y))
+test_data = test_data.batch(batch_size)
+test_data = test_data.prefetch(tf.data.AUTOTUNE)
+
+valid_data = valid_data.map(lambda x, y: (tf.image.central_crop(x, central_fraction=0.8), y))
+valid_data = valid_data.map(lambda x, y: (tf.image.resize(x, (224,224)), y))
+valid_data = valid_data.batch(batch_size)
+valid_data = valid_data.prefetch(tf.data.AUTOTUNE)
+
+class ResNetBlock(Layer):
+
+  def __init__(self, out_channels, first_stride=1):
+    super().__init__()
+
+    first_padding = 'same'
+    if first_stride != 1:
+      first_padding = 'valid'
     
-IMAGE_RES = 224
-def format_image(image, label):
-  image = tf.image.resize(image, (IMAGE_RES, IMAGE_RES))/255.0
-  return image, label
+    self.conv_sequence = Sequential([
+        Conv2D(out_channels, 3, first_stride, padding=first_padding),
+        BatchNormalization(),
+        ReLU(),
 
-BATCH_SIZE = 32
+        Conv2D(out_channels, 3, 1, padding='same'),
+        BatchNormalization(),
+        ReLU()
+    ])
 
-train_batches = train_ds.cache().shuffle(len(train_ds)//4).map(format_image).batch(BATCH_SIZE).prefetch(1)
+  def call(self, inputs):
+    x = self.conv_sequence(inputs)
 
-validation_batches = validate_ds.cache().map(format_image).batch(BATCH_SIZE).prefetch(1)
-
-test_batches = test_ds.cache().map(format_image).batch(BATCH_SIZE).prefetch(1)
-
-model=keras.models.Sequential([
-    keras.layers.Conv2D(filters=128, kernel_size=(15,15), strides=(4,4), activation='relu', input_shape=(224,224,3)),
-    keras.layers.BatchNormalization(),
-    keras.layers.MaxPool2D(pool_size=(2,2)),
-    keras.layers.Conv2D(filters=256, kernel_size=(5,5), strides=(1,1), activation='relu', padding="same"),
-    keras.layers.BatchNormalization(),
-    keras.layers.MaxPool2D(pool_size=(3,3)),
-    keras.layers.Conv2D(filters=256, kernel_size=(3,3), strides=(1,1), activation='relu', padding="same"),
-    keras.layers.Conv2D(filters=256, kernel_size=(3,3), strides=(1,1), activation='relu', padding="same"),
-    keras.layers.BatchNormalization(),
-    keras.layers.Conv2D(filters=512, kernel_size=(3,3), strides=(1,1), activation='relu', padding="same"),
-    keras.layers.BatchNormalization(),
-    keras.layers.Conv2D(filters=512, kernel_size=(3,3), strides=(1,1), activation='relu', padding="same"),
-    keras.layers.BatchNormalization(),
-    keras.layers.MaxPool2D(pool_size=(2,2)),
-    keras.layers.Flatten(),
-    keras.layers.Dense(1024,activation='relu'),
-    keras.layers.Dropout(0.5),
-    keras.layers.Dense(1024,activation='relu'),
-    keras.layers.Dropout(0.5),
-    keras.layers.Dense(102,activation='softmax')  
+    if x.shape == inputs.shape:
+      x = x + inputs # Skip connection
     
+    return x
+
+layer = ResNetBlock(4)
+print(layer)
+
+class ResNet(Model):
+  def __init__(self):
+    super(ResNet, self).__init__()
+
+    self.conv_1 = Sequential([
+                              Conv2D(64, 7, 2),
+                              ReLU(),
+                              MaxPooling2D(3, 2)
+    ])
+
+    self.resnet_chains = Sequential([ResNetBlock(64), ResNetBlock(64)] +
+                                    [ResNetBlock(128, 2), ResNetBlock(128)] +
+                                    [ResNetBlock(256, 2), ResNetBlock(256)] +
+                                    [ResNetBlock(512, 2), ResNetBlock(512)] 
+                                    )
     
-])
-
-model.compile(
-    loss='sparse_categorical_crossentropy',
-    optimizer='SGD',
-    metrics=['accuracy'],
+    self.out = Sequential([GlobalAveragePooling2D(),
+                           Dropout(0.8),
+                           Dense(102, activation='softmax')])
     
-)
-model.summary()
+  def call(self, x):
+    x = self.conv_1(x)
+    x = self.resnet_chains(x)
+    x = self.out(x)
+    return x
+
+model = ResNet()
+print(model)
 
 
-history = model.fit(train_batches,
-                    epochs=100,
-                    validation_data=validation_batches,
-                    validation_freq=1,
-                   )
 
-f,ax=plt.subplots(2,1,figsize=(10,10)) 
+model.compile(optimizer='SGD', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
 
-#Assigning the first subplot to graph training loss and validation loss
-ax[0].plot(model.history.history['loss'],color='b',label='Training Loss')
-ax[0].plot(model.history.history['val_loss'],color='r',label='Validation Loss')
+class CustomCallback(tf.keras.callbacks.Callback):
+  def on_epoch_end(self, epoch, logs=None):
+    if logs.get('val_accuracy') >= 0.53 :
+      self.model.stop_training = True
 
-#Plotting the training accuracy and validation accuracy
-ax[1].plot(model.history.history['accuracy'],color='b',label='Training  Accuracy')
-ax[1].plot(model.history.history['val_accuracy'],color='r',label='Validation Accuracy')
+callback = CustomCallback()
 
-plt.legend()
+callback = CustomCallback()
+model.fit(train_data, epochs=300,validation_data = valid_data,validation_freq =1,callbacks=[callback])
 
-model.evaluate(test_batches)
+model.evaluate(test_data)
